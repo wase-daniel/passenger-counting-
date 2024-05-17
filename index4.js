c663cbc7c43d1fd657d8df738bce31d0f364d0f2
daniel wase <wasedaniel01@gmail.com>
2024-05-17 18:42:57 +0000

    this display 42 when you clicked the buuton

--- index4.js
+++ index4.js
@@ -1,5 +1,12 @@
 //document.getElementById("c").innerText = 7
 
-  function increment() {
-    console.log("the button was clicked h")
+  // function increment() {
+  //   console.log("the button was clicked ")
+  // }
+
+  function dan(ef) {
+   
+    console.log(42)
+
   }
+dan()
