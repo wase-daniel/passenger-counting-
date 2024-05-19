97d009725a2bd896aef67fa4e7937c025394bc36
daniel wase <wasedaniel01@gmail.com>
2024-05-19 09:51:26 +0000

    my button  working on the main page

--- index4.js
+++ index4.js
@@ -1,10 +1,20 @@
 //document.getElementById("c").innerText = 7//
+countEl = document.getElementById("c")
+
+console.log(countEl)
+
+
+
+
+
 let lops = 0
 
 function increment(){
  
    lops ++
-   console.log(lops  ) 
+  countEl.innerText = lops
+
+   
 
 }
  
